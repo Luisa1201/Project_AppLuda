fb581cd3ff2ff984ae811f63eacb355f390c8ab5
Luisa1201 <lfacostav@ufpso.edu.co>
2025-10-03 19:06:14 -0500

    Interfaz de App Luda

---
+++ app/src/main/java/com/example/appluda/SupportActivity.kt
@@ -0,0 +1,11 @@
+package com.example.appluda
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SupportActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_support)
+    }
+}
